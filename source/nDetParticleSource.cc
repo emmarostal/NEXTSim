49a0439d009f2de7f6b4a2b23db1662e1539f4a2
jheidema <jheid1205@gmail.com>
2021-12-01 14:43:08 -0500

    Recovering personal edits

--- source/nDetParticleSource.cc
+++ source/nDetParticleSource.cc
@@ -366,7 +366,9 @@ bool nDetParticleSource::ReadEnergyFile(const char *filename){
 		Display::ErrorPrint("Failed to open input energy distribution file.", "nDetParticleSource");
 		return false;
 	}
-
+	
+	// Set to a neutron
+	GetCurrentSource()->SetParticleDefinition(G4Neutron::NeutronDefinition());  //hard coded for now - JNH
 	G4SPSEneDistribution *ene = GetCurrentSource()->GetEneDist();
 	ene->SetEnergyDisType("Arb");
 	
